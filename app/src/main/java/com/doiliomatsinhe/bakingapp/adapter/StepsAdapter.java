f76eed8825ab5eaf3f012141ae15a07798bf4b8f
doilio <doiliomatsinhe@hotmail.com>
2020-04-05 16:17:34 +0200

    Add Steps
    Signed-off-by: doilio <doiliomatsinhe@hotmail.com>

---
+++ app/src/main/java/com/doiliomatsinhe/bakingapp/adapter/StepsAdapter.java
@@ -0,0 +1,80 @@
+package com.doiliomatsinhe.bakingapp.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.doiliomatsinhe.bakingapp.databinding.StepItemBinding;
+import com.doiliomatsinhe.bakingapp.model.Step;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StepsAdapter extends RecyclerView.Adapter<StepsAdapter.ViewHolder> {
+
+    private List<Step> stepsList = new ArrayList<>();
+    private StepsAdapter.StepsItemClickListener onClickListener;
+
+    public StepsAdapter(StepsItemClickListener onClickListener) {
+        this.onClickListener = onClickListener;
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        private StepItemBinding binding;
+
+        public ViewHolder(@NonNull StepItemBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+            binding.getRoot().setOnClickListener(this);
+        }
+
+        void bind(Step item) {
+            int stepNr = item.getId() + 1;
+            String description = item.getShortDescription();
+
+            binding.stepNumber.setText(String.format("Step %s", stepNr));
+            binding.stepDescription.setText(description);
+
+            binding.executePendingBindings();
+        }
+
+        @Override
+        public void onClick(View v) {
+            int clickedItem = getAdapterPosition();
+            onClickListener.onStepsItemClick(clickedItem);
+        }
+    }
+
+    public void setStepsList(List<Step> stepsList) {
+        this.stepsList = stepsList;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public StepsAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        StepItemBinding binding = StepItemBinding.inflate(inflater, parent, false);
+        return new ViewHolder(binding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull StepsAdapter.ViewHolder holder, int position) {
+        Step currentStep = stepsList.get(position);
+        holder.bind(currentStep);
+    }
+
+    @Override
+    public int getItemCount() {
+        return stepsList.size();
+    }
+
+    public interface StepsItemClickListener {
+        void onStepsItemClick(int position);
+    }
+
+
+}
--- app/src/main/java/com/doiliomatsinhe/bakingapp/ui/recipeDetail/RecipeDetailActivity.java
+++ app/src/main/java/com/doiliomatsinhe/bakingapp/ui/recipeDetail/RecipeDetailActivity.java
@@ -2,30 +2,42 @@ package com.doiliomatsinhe.bakingapp.ui.recipeDetail;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.doiliomatsinhe.bakingapp.R;
+import com.doiliomatsinhe.bakingapp.adapter.RecipeAdapter;
+import com.doiliomatsinhe.bakingapp.adapter.StepsAdapter;
 import com.doiliomatsinhe.bakingapp.databinding.ActivityRecipeDetailBinding;
 import com.doiliomatsinhe.bakingapp.model.Ingredient;
 import com.doiliomatsinhe.bakingapp.model.Recipe;
+import com.doiliomatsinhe.bakingapp.model.Step;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import static com.doiliomatsinhe.bakingapp.ui.recipe.RecipeActivity.RECIPE;
 
-public class RecipeDetailActivity extends AppCompatActivity {
+public class RecipeDetailActivity extends AppCompatActivity implements StepsAdapter.StepsItemClickListener {
 
     private static final String TAG = RecipeDetailActivity.class.getSimpleName();
     private ActivityRecipeDetailBinding binding;
     private Recipe recipe;
+    private List<Step> stepsList = new ArrayList<>();
+    private StepsAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_recipe_detail);
+
+        initComponents();
 
         Intent i = getIntent();
         if (i.getSerializableExtra(RECIPE) != null) {
@@ -41,9 +53,25 @@ public class RecipeDetailActivity extends AppCompatActivity {
 
     }
 
+    private void initComponents() {
+
+        // Adapter
+        adapter = new StepsAdapter(this);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        binding.recyclerStep.setLayoutManager(layoutManager);
+        binding.recyclerStep.setHasFixedSize(true);
+        binding.recyclerStep.setAdapter(adapter);
+        binding.recyclerStep.addItemDecoration(new DividerItemDecoration(this,DividerItemDecoration.VERTICAL));
+    }
+
     private void populateUI(Recipe recipe) {
 
         if (recipe != null) {
+
+            // Setting up the list of steps
+            stepsList = recipe.getSteps();
+
+            adapter.setStepsList(stepsList);
 
             // Setting up the ActionBar
             Objects.requireNonNull(getSupportActionBar()).setTitle(recipe.getName());
@@ -55,11 +83,11 @@ public class RecipeDetailActivity extends AppCompatActivity {
                 String unit = ingredient.getMeasure();
                 float quantity = ingredient.getQuantity();
 
-                String text ="";
-                if (quantity % 1 == 0){
-                    text = String.format("* %s %s of %s\n", (int)quantity, unit, name);
-                }else{
-                     text = String.format("* %s %s of %s\n", quantity, unit, name);
+                String text = "";
+                if (quantity % 1 == 0) {
+                    text = String.format("* %s %s of %s\n", (int) quantity, unit, name);
+                } else {
+                    text = String.format("* %s %s of %s\n", quantity, unit, name);
                 }
 
                 stringBuilder.append(text);
@@ -69,5 +97,12 @@ public class RecipeDetailActivity extends AppCompatActivity {
             binding.ingredientsText.setText(finalString);
             Log.d(TAG, "Texto: \n" + finalString);
         }
+    }
+
+    @Override
+    public void onStepsItemClick(int position) {
+        Step step = stepsList.get(position);
+
+        Toast.makeText(this, "Clicked: " + step.getShortDescription(), Toast.LENGTH_SHORT).show();
     }
 }
